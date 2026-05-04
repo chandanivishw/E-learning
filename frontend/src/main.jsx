32529cbc2261b97d36391c00d8ba3275679dc739
chandanivishw <vishwakarmachandani400@gmail.com>
2026-05-04 22:16:35 +0530

    fix context import case issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { UserContextProvider } from './Context/UserContext.jsx';
-import { CourseContextProvider } from './context/CourseContext.jsx';
+import { CourseContextProvider } from './Context/CourseContext.jsx';
 
 
 export const server = "http://localhost:5000";
